54e0603798ae502563f8b951abd89dc150ef3af2
Davlen14 <99432647+Davlen14@users.noreply.github.com>
2024-09-06 21:38:31 -0500

    Updated live stats component

---
+++ GamedayApp/PlayByPlayData.swift
@@ -0,0 +1,8 @@
+//
+//  PlayByPlayData.swift
+//  GamedayApp
+//
+//  Created by Davlen Swain on 9/6/24.
+//
+
+import Foundation
---
+++ GamedayApp/PlayByPlayView.swift
@@ -0,0 +1,18 @@
+//
+//  PlayByPlayView.swift
+//  GamedayApp
+//
+//  Created by Davlen Swain on 9/6/24.
+//
+
+import SwiftUI
+
+struct PlayByPlayView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlayByPlayView()
+}
